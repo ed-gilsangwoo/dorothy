aa6cb0f55dfbd97bba92303634de6e040da698c4
taeiim <ti535666@gmail.com>
2017-10-01 01:32:13 +0900

    Login and SignUp xml

--- Dorothy/app/src/main/java/com/example/parktaeim/dorothy/Activity/LoginActivity.java
+++ Dorothy/app/src/main/java/com/example/parktaeim/dorothy/Activity/LoginActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.parktaeim.dorothy.R;
+
 /**
  * Created by parktaeim on 2017. 10. 1..
  */
@@ -12,5 +14,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
     }
 }
