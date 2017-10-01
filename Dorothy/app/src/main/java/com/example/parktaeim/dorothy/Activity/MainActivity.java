8cfda5c0f1732b71152b678949b896243ed84cac
taeiim <ti535666@gmail.com>
2017-10-01 12:20:08 +0900

    add Daum Maps API

--- Dorothy/app/src/main/java/com/example/parktaeim/dorothy/Activity/MainActivity.java
+++ Dorothy/app/src/main/java/com/example/parktaeim/dorothy/Activity/MainActivity.java
@@ -1,9 +1,23 @@
 package com.example.parktaeim.dorothy.Activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
+
 
 import com.example.parktaeim.dorothy.R;
+
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
+
+
+
