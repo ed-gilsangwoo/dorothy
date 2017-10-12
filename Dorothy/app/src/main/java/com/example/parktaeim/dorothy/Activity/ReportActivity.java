9e3d569a80270a057be0df4cc14e40010791a729
Terry Yun <nooheat1228@gmail.com>
2017-10-13 01:09:34 +0900

    [DOING] report

--- Dorothy/app/src/main/java/com/example/parktaeim/dorothy/Activity/ReportActivity.java
+++ Dorothy/app/src/main/java/com/example/parktaeim/dorothy/Activity/ReportActivity.java
@@ -19,51 +19,91 @@ import com.kofigyan.stateprogressbar.StateProgressBar;
  * Created by parktaeim on 2017. 10. 6..
  */
 
-public class ReportActivity extends AppCompatActivity implements View.OnClickListener{
-    private boolean btn1Pressed = false, btn2Pressed = false, btn3Pressed = false;
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private StateProgressBar progressBar;
-    String[] descData = {"1단계","2단계","3단계"};
+public class ReportActivity extends AppCompatActivity {
+
+    private StateProgressBar strengthBar;
+
+    private String[] descData = {"1단계", "2단계", "3단계"};
+
+    private Button stateButtons[] = new Button[3];
+    private int state = 1;
+
+    private Button strengthButtons[] = new Button[3];
+    private int strength = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        progressBar = (StateProgressBar) findViewById(R.id.progress_bar);
+        initStrengthBar();
+        initStateButtons();
+    }
 
-        progressBar.setStateDescriptionData(descData);
+    private void initStrengthBar() {
+        strengthBar = (StateProgressBar) findViewById(R.id.progress_bar);
+        strengthBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+        strengthBar.setStateDescriptionData(descData);
+        strengthBar.enableAnimationToCurrentState(true);
 
-        button1.setOnClickListener((View.OnClickListener)this);
-        button2.setOnClickListener((View.OnClickListener)this);
-        button3.setOnClickListener((View.OnClickListener)this);
+        strengthButtons[0] = (Button) findViewById(R.id.strength1);
+        strengthButtons[1] = (Button) findViewById(R.id.strength2);
+        strengthButtons[2] = (Button) findViewById(R.id.strength3);
 
-
-        progressBar.setOnClickListener(new View.OnClickListener() {
+        strengthButtons[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("view----------",view.toString());
+                strength = 1;
+                strengthBar.setCurrentStateNumber(StateProgressBar.StateNumber.ONE);
+                Toast.makeText(getApplicationContext(), strength + "", Toast.LENGTH_SHORT).show();
             }
         });
 
+        strengthButtons[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strength = 2;
+                strengthBar.setCurrentStateNumber(StateProgressBar.StateNumber.TWO);
+                Toast.makeText(getApplicationContext(), strength + "", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        strengthButtons[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strength = 3;
+                strengthBar.setCurrentStateNumber(StateProgressBar.StateNumber.THREE);
+                Toast.makeText(getApplicationContext(), strength + "", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private void initStateButtons() {
+        stateButtons[0] = (Button) findViewById(R.id.btn_roadDamage);
+        stateButtons[1] = (Button) findViewById(R.id.btn_flood);
+        stateButtons[2] = (Button) findViewById(R.id.btn_landslide);
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.button1:
-                break;
-            case R.id.button2:
-                break;
-            case R.id.button3:
-                break;
+        stateButtons[0].setBackgroundColor(getResources().getColor(R.color.colorPrimary, getTheme()));
+        stateButtons[0].setTextColor(getResources().getColor(android.R.color.white, getTheme()));
+
+        for (int i = 0; i < stateButtons.length; i++) {
+            final int finalI = i;
+            stateButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    state = finalI + 1;
+                    for (int j = 0; j < 3; j++) {
+                        if (j + 1 == state) {
+                            stateButtons[j].setBackgroundColor(getResources().getColor(R.color.colorPrimary, getTheme()));
+                            stateButtons[j].setTextColor(getResources().getColor(android.R.color.white, getTheme()));
+                        } else {
+                            stateButtons[j].setBackgroundColor(getResources().getColor(android.R.color.white, getTheme()));
+                            stateButtons[j].setTextColor(getResources().getColor(R.color.gray, getTheme()));
+                        }
+                    }
+                    Toast.makeText(getApplicationContext(), state + "", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
